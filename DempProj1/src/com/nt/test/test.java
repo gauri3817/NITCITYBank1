52ecea6dc0b058c01d05e481e836b02502717aaf
TL <TL@gmail.com>
2022-01-21 09:47:49 +0530

    dev1 modified test.java

--- DempProj1/src/com/nt/test/test.java
+++ DempProj1/src/com/nt/test/test.java
@@ -1,7 +1,13 @@
 package com.nt.test;
 
+import com.nt.beans.Utility1;
+
 public class test {
-	
+	public static void main(String args[]) {
+		Utility1 u1=new Utility1();
+		System.out.println(u1.getSysDate());
+		
+	}
 
 
 }
